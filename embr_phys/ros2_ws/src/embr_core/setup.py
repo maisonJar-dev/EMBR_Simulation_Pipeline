a22a3e1cf31059800917d442182c4959bbe11cdb
maisonjar-dev <maison.personal03@gmail.com>
2026-08-05 21:04:09 -0700

    Fixed dependency ownership issue with python3-serial

--- embr_phys/ros2_ws/src/embr_core/setup.py
+++ embr_phys/ros2_ws/src/embr_core/setup.py
@@ -15,7 +15,7 @@ setup(
         ),
         ("share/" + package_name, ["package.xml", "IBUS_SETUP.md"]),
     ],
-    install_requires=["setuptools", "pyserial"],
+    install_requires=["setuptools"],
     zip_safe=True,
     maintainer="Maison Gulyas",
     maintainer_email="maison.personal03@gmail.com",
