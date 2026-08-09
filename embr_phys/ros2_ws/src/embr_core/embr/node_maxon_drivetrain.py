5194d838d8c9990687f0699d69806943c03d2fb2
maisonjar-dev <maison.personal03@gmail.com>
2026-08-08 22:01:46 -0700

    Added subscriber node for maxon control system

--- embr_phys/ros2_ws/src/embr_core/embr/node_maxon_drivetrain.py
+++ embr_phys/ros2_ws/src/embr_core/embr/node_maxon_drivetrain.py
@@ -1,1 +1,40 @@
 # TODO: Should handle the control logic of the maxon motors 
+
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import Float32MultiArray
+
+class MaxonTeleopControlSystem(Node):
+
+    def __init__(self):
+        super().__init__('maxon_teleop_control_system')
+
+        self._motor_subscriber = self.create_subscription(
+            Float32MultiArray,
+            'motor_velocity_levels',
+            self.motor_velocity_callback,
+            10
+        )
+
+        self._motor_subscriber # Avoid unused variable warning
+
+    def motor_velocity_callback(self, msg):
+        self.get_logger().info("Motor velocities: %s" % msg.data)
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = MaxonTeleopControlSystem()
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        if rclpy.ok():
+            rclpy.shutdown()
+    
+if __name__ == "__main__":
+    main()
